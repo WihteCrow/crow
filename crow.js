d0e8c799466c7616d4b428d4ffd5850118c19c83
乌鸦 <yangchen.xiyou@gmail.com>
2014-09-10 00:54:37 +0800

    crow 项目初始化

---
+++ crow.js
@@ -0,0 +1,23 @@
+/**
+ * Created by 乌鸦 on 2014/9/10.
+ *
+ * 建立自己的库
+ *
+ * jquery-2.1.1.min.js 放弃IE6、7、8
+ */
+
+(function($, window){
+
+    var Crow = function(){
+        if(this instanceof Crow){
+            this.version = '0.1';
+            this.author = 'Crow';
+            this.time = '2014/9/10';
+        }else{
+            return new Crow();
+        }
+    };
+
+    window.crow = new Crow;
+
+})(jQuery, window);
